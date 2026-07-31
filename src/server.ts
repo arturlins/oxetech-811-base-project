62306aaac05f1eaee3451088a689bba2a9982999
Artur Lins <artur.olins@gmail.com>
2026-07-31 18:38:10 -0300

    feat(logging): adiciona middleware de logging de requisicoes HTTP

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import express from "express";
 import "dotenv/config";
 import router from "./routes";
 import { errorHandler } from "./middlewares/error.middleware";
+import { requestLogger } from "./middlewares/logger.middleware";
 
 const app = express();
 const port = Number(process.env.PORT || 3000);
 
 app.use(cors());
 app.use(express.json());
+app.use(requestLogger);
 app.use("/api", router);
 
 app.use((_request, response) => {
